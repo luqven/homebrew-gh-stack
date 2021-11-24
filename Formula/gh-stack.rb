b93836164e3ac24891ca4125f640e55e2520e14b
Luis Ball <lball@imgix.com>
2021-11-24 10:29:21 -0800

    chore: release v0.3.0-beta

--- Formula/gh-stack.rb
+++ Formula/gh-stack.rb
@@ -1,9 +1,9 @@
 class GhStack < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/luqven/gh-stack"
-  version "v0.2.0-beta"
+  version "v0.3.0-beta"
   url "https://github.com/luqven/gh-stack/releases/download/#{version}/gh-stack-mac.tar.gz"
-  sha256 "e46f74406b55b3f17df2963c424e9ed11eb37723a3578239189ac38638f88fdc"
+  sha256 "fb46a48e0ab9f738a84b1a87f1da1aefd7c3905a9b5e4ddd4a8f5ad4bf54ea88"
 
   def install
     bin.install "gh-stack"
