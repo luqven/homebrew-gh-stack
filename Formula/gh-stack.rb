2a2746e98a2610aba38a3b53e9282524ff6b1b92
Luis H. Ball Jr. <contact.luis@icloud.com>
2025-12-29 13:49:15 -0800

    bump gh-stack to v0.6.0 (#4)
    Co-authored-by: luqven <luqven@users.noreply.github.com>

--- Formula/gh-stack.rb
+++ Formula/gh-stack.rb
@@ -1,9 +1,9 @@
 class GhStack < Formula
   desc "Manage PR stacks/chains on Github"
   homepage "https://github.com/luqven/gh-stack"
-  version "v0.5.1"
+  version "v0.6.0"
   url "https://github.com/luqven/gh-stack/releases/download/#{version}/gh-stack-mac.tar.gz"
-  sha256 "eff506afd58425cf2eed8247d2b383d8306a8b58359460ca5f6522e03a1f73fe"
+  sha256 "70342a5a6d46438461f28933cfd5ea95e9c3d20993c307de67d4a645f06ca1e7"
 
   def install
     bin.install "gh-stack"
